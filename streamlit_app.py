9c21b45e93a3b8accd399f0619cd71a5e219bbda
Embruh69 <93874399+Embruh69@users.noreply.github.com>
2023-10-12 19:46:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import streamlit as st
 import tensorflow as tf
 import keras
@@ -13,32 +7,18 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-
-# In[7]:
-
-
 model = tf.keras.models.load_model('./model.keras')
+model.load_weights('./balancing.h5')
 
-
-# In[ ]:
-
-
-st.title('Clickbait Detection App')
-user_input = st.text_input('Enter Video Title')
-
-
-# In[ ]:
 maxlen = 500
 vocab_size = 5000
 tokenizer = Tokenizer(num_words=vocab_size)
 tokenizer.fit_on_texts([user_input])
 
-
-if st.button('Detect Clickbait'):
-    test = [user_input]
-    token_text = pad_sequences(tokenizer.texts_to_sequences(test), maxlen=maxlen)
-    preds = [round(i[0]) for i in model.predict(token_text)]
-    for (text, pred) in zip(test, preds):
-        label = 'Clickbait' if pred == 1.0 else 'Not Clickbait'
-        st.write(f'The video title "{user_input}" is most likely: {label}')
-
+test = [user_input]
+token_text = pad_sequences(tokenizer.texts_to_sequences(test), maxlen=maxlen)
+preds = [round(i[0]) for i in model.predict(token_text)]
+for (text, pred) in zip(test, preds):
+    label = 'Clickbait' if pred == 1.0 else 'Not Clickbait'
+    print("{} - {}".format(text, label))
+    st.write(f'The video title "{user_input}" is most likely: {pred_label}')
